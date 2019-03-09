8b6c6c8ad197fda455f1ef4416b236ca0d2f697a
github username <github email address>
2019-03-09 16:38:13 -0700

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,22 @@
 # Code your solution here!
+require 'pry'
+#Must start game with this method according to rspec test
+def run_guessing_game
+  user_guess = 0
+  puts "Guess a number between 1 and 6."
+  user_guess = gets.chomp
+
+  while user_guess != "exit"
+
+    rand_num = rand(1..6)
+    if user_guess.to_i == rand_num
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{rand_num}."
+    end
+
+    puts "Guess a number between 1 and 6"
+    user_guess = gets.chomp
+  end
+  puts "Goodbye!"
+end
